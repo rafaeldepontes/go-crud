a917d2d220a2561c2c05747d6c5cdffd3e349633
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2025-11-22 01:53:51 -0300

    feat: adding first steps into the database layer

---
+++ internal/tools/database.go
@@ -0,0 +1,36 @@
+package tool
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+type LoginDetails struct {
+	AuthToken string
+	Username string
+}
+type BalanceDetails struct {
+	Balance float64
+	Username string
+}
+
+type DatabaseInterface interface {
+	GetUserLoginDetails(username string) *LoginDetails
+	GetUserBalance(username string) *BalanceDetails
+	SetUpDatabase() error
+}
+
+func NewDataBase() (*DatabaseInterface, error) {
+
+	var database DatabaseInterface = &mockDB{} // Need to implement this...
+	var err error = database.SetUpDatabase()
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return database, nil
+}
+
+func GetUserLoginDetails(username string) {
+
+}
